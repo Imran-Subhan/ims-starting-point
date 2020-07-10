36761ac0147210cab51ad6222001e711d0534a51
Imran-Subhan <isubhan@qa.com>
2020-07-10 12:21:01 +0100

    customer fields added

--- src/main/java/com/qa/ims/persistence/domain/Customer.java
+++ src/main/java/com/qa/ims/persistence/domain/Customer.java
@@ -2,56 +2,112 @@ package com.qa.ims.persistence.domain;
 
 public class Customer {
 
-	private Long id;
-	private String firstName;
-	private String surname;
+	private Long customerID;
+	private String FullName;
+	private String email;
+	private Long Mobile;
+	private String Address;
+	private String PostCode;
 
-	public Customer(String firstName, String surname) {
-		this.firstName = firstName;
-		this.surname = surname;
+
+	public Customer(Long id, String FullName, String email, Long Mobile, String Address, String PostCode) {
+		this.customerID = id;
+		this.FullName = FullName;
+		this.email = email;
+		this.Mobile = Mobile;
+		this.Address = Address;
+		this.PostCode = PostCode;
 	}
 
-	public Customer(Long id, String firstName, String surname) {
-		this.id = id;
-		this.firstName = firstName;
-		this.surname = surname;
+
+
+	public Long getcustomerID() {
+		return customerID;
 	}
 
-	public Long getId() {
-		return id;
+
+
+	public void setcustomerID(Long customerID) {
+		this.customerID = customerID;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+
+
+	public String getFullName() {
+		return FullName;
 	}
 
-	public String getFirstName() {
-		return firstName;
+
+
+	public void setFullName(String FullName) {
+		this.FullName = FullName;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+
+
+	public String getEmail() {
+		return email;
 	}
 
-	public String getSurname() {
-		return surname;
+
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
-	public void setSurname(String surname) {
-		this.surname = surname;
+
+
+	public Long getMobile() {
+		return Mobile;
 	}
+
+
+
+	public void setMobile(Long Mobile) {
+		this.Mobile = Mobile;
+	}
+
+
+
+	public String getAddress() {
+		return Address;
+	}
+
+
+
+	public void setAddress(String Address) {
+		this.Address = Address;
+	}
+
+
+
+	public String getPostCode() {
+		return PostCode;
+	}
+
+
+
+	public void setPostCode(String PostCode) {
+		this.PostCode = PostCode;
+	}
+
+
 
 	public String toString() {
-		return "id:" + id + " first name:" + firstName + " surname:" + surname;
+		return "CustomerID:" + customerID + " Full name:" + FullName + " e-mail:" + email + " Mobile:" + Mobile + " Address:" + Address + " Post Code:" + PostCode;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		result = prime * result + ((FullName == null) ? 0 : FullName.hashCode());
+		result = prime * result + ((customerID == null) ? 0 : customerID.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((Mobile == null) ? 0 : Mobile.hashCode());
+		result = prime * result + ((Address == null) ? 0 : Address.hashCode());
+		result = prime * result + ((PostCode == null) ? 0 : PostCode.hashCode());
+		
 		return result;
 	}
 
@@ -64,21 +120,21 @@ public class Customer {
 		if (getClass() != obj.getClass())
 			return false;
 		Customer other = (Customer) obj;
-		if (firstName == null) {
-			if (other.firstName != null)
+		if (FullName == null) {
+			if (other.FullName != null)
 				return false;
-		} else if (!firstName.equals(other.firstName))
+		} else if (!FullName.equals(other.FullName))
 			return false;
-		if (id == null) {
-			if (other.id != null)
+		if (customerID == null) {
+			if (other.customerID != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!customerID.equals(other.customerID))
 			return false;
-		if (surname == null) {
-			if (other.surname != null)
-				return false;
-		} else if (!surname.equals(other.surname))
-			return false;
+//		if (surname == null) {
+//			if (other.surname != null)
+//				return false;
+//		} else if (!surname.equals(other.surname))
+//			return false;
 		return true;
 	}
 
