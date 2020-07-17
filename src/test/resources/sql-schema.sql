442f6d0e7a881ac63496f5308764fe6196dcb6d5
Imran-Subhan <isubhan@qa.com>
2020-07-17 13:19:05 +0100

    fix

--- src/test/resources/sql-schema.sql
+++
@@ -1,4 +0,0 @@
-create database if not exists ims_test;
-create table if not exists ims_test.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
-create table if not exists ims_test.items(productid int primary key auto_increment, productName varchar(40), price int);
-create table if not exists ims_test.basket(orderitemsid int primary key auto_increment, orderid int, customerid int, productid int, quantity int, price float);
